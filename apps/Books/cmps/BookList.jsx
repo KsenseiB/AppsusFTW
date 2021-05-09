6a83e36ea5e235f88581941705dff925926498a1
Ksenia <ksu0593@gmail.com>
2021-05-09 08:58:21 +0300

    books dont work

--- apps/Books/cmps/BookList.jsx
+++ apps/Books/cmps/BookList.jsx
@@ -2,7 +2,7 @@ import {BookPreview} from '../cmps/BookPreview.jsx'
 
 export function BookList({books, setSelectedBook}){
     return(
-        <div className="book-list">
+        <div className="book-list flex">
             {books.map((book) =>(
             <BookPreview book={book} key={book.id} setSelectedBook={setSelectedBook}/>            
             ))}
